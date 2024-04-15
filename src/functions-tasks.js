425a807d44882a5a1c3055a0b90e68f3e518e712
Anton Ramarchuk <anton.ramarchuk@sahaty.local>
2024-04-15 20:55:58 +0200

    feat: add solution 10

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -209,8 +209,10 @@ function logger(func, logFunc) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return (...moreArgs) => {
+    return fn(...args1, ...moreArgs);
+  };
 }
 
 /**
