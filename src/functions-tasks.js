70fc3642fa94f1f40c91dba90c247202a2d136fa
Anton Ramarchuk <anton.ramarchuk@sahaty.local>
2024-04-15 20:47:11 +0200

    feat: add solution 6

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -117,8 +117,17 @@ function getPolynom(...coefficients) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  let memo = null;
+  let hasResult = false;
+
+  return () => {
+    if (!hasResult) {
+      memo = func();
+      hasResult = true;
+    }
+    return memo;
+  };
 }
 
 /**
